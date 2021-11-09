2357b1c9576661e1e2b3d9aa19bc166996fcf720
Desiree Mendes <88469223+desireemendes@users.noreply.github.com>
2021-11-09 18:37:58 -0500

    login and logout endpoints (#1)

--- express_server.js
+++ express_server.js
@@ -2,7 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-
+const cookieParser = require('cookie-parser')
+app.use(cookieParser())
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +19,7 @@ function generateRandomString() {
 
 app.get("/", (req, res) => {
   res.send("Hello!");
+  // res.redirect(`/urls/`)
 });
 
 app.listen(PORT, () => {
@@ -33,12 +35,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,
+  username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { urls: urlDatabase,
+    username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
@@ -61,10 +66,23 @@ app.get("/urls/:shortURL", (req, res) => {
     res.status(400);
     res.send("Page not found");
   } else {
-    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+    const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"] };
     res.render("urls_show", templateVars);
   }
 });
+
+app.post('/login', (req, res) => {
+  const username = req.body.username
+  res.cookie('username', username);
+  res.redirect(`/urls`);
+});
+
+app.post('/logout', (req, res) => {
+  const username = req.body.username
+  res.clearCookie('username', username)
+  res.redirect(`/urls`);
+})
 
 
 app.post('/urls/:shortURL/delete', (req, res) => {
@@ -72,6 +90,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect(`/urls`);
 });
 
+//update URL's
 app.post('/urls/:shortURL', (req, res) => {
   if (urlDatabase[req.params.shortURL]) {
     urlDatabase[req.params.shortURL] = req.body.longURL;
